5371d14c87a70e2c8ca7e2bc5c20c94e7d31c23d
Cai heng <ianyaheng822@gmail.com>
2020-03-05 15:45:47 +1100

    add the dashboard

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -2,22 +2,34 @@
 
 @section('content')
 <div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">Dashboard</div>
-
-                <div class="card-body">
-                    @if (session('status'))
-                        <div class="alert alert-success" role="alert">
-                            {{ session('status') }}
-                        </div>
-                    @endif
-
-                    You are logged in!
-                </div>
+    <div class="row">
+        <div class="col-3 p-5">
+            <img alt="freecodecamp's profile picture" class="_6q-tv rounded-circle"  style="height:150px; " src="https://instagram.fsyd3-1.fna.fbcdn.net/v/t51.2885-19/s320x320/83213956_3360255157381124_5752385570823208960_n.jpg?_nc_ht=instagram.fsyd3-1.fna.fbcdn.net&amp;_nc_ohc=qCRDegvAcZkAX-AqLs_&amp;oh=e5be214795d60ffd05c35a87ac8d211b&amp;oe=5E91EDC2">
+        </div>
+        <div class="col-9">
+            <div><h1>CodeCamp</h1></div>
+            <div class="d-flex">
+                <div class="pr-5"><strong>150 </strong>posts</div>
+                <div class="pr-5"><strong>23k </strong>folowers</div>
+                <div class="pr-5"><strong>234 </strong>following</div>
             </div>
+            <div class="pt-4 font-wight-blod">freeCodeCamp.org</div>
+            <div>We're a global community of millions of people learning to code together. We're an open source, donor-supported, 501(c)(3) nonprofit.</div>
+            <div><a href="https://www.freecodecamp.org/">www.freecodecamp.org</a></div>
         </div>
     </div>
+
+    <div class="row">
+        <div class="col-4">
+            <img src="https://instagram.fsyd3-1.fna.fbcdn.net/v/t51.2885-15/e35/c100.0.501.501a/89260743_2658694764411607_948672185790002051_n.jpg?_nc_ht=instagram.fsyd3-1.fna.fbcdn.net&amp;_nc_cat=107&amp;_nc_ohc=n9Nz7_LWpu4AX-7vp9j&amp;oh=9daabf83f369803b4cac13a2cbcb24e5&amp;oe=5E982F5D" alt="">
+            <img src="https://instagram.fsyd3-1.fna.fbcdn.net/v/t51.2885-15/e35/c100.0.501.501a/89260743_2658694764411607_948672185790002051_n.jpg?_nc_ht=instagram.fsyd3-1.fna.fbcdn.net&amp;_nc_cat=107&amp;_nc_ohc=n9Nz7_LWpu4AX-7vp9j&amp;oh=9daabf83f369803b4cac13a2cbcb24e5&amp;oe=5E982F5D" alt="">
+            <img src="https://instagram.fsyd3-1.fna.fbcdn.net/v/t51.2885-15/e35/c100.0.501.501a/89260743_2658694764411607_948672185790002051_n.jpg?_nc_ht=instagram.fsyd3-1.fna.fbcdn.net&amp;_nc_cat=107&amp;_nc_ohc=n9Nz7_LWpu4AX-7vp9j&amp;oh=9daabf83f369803b4cac13a2cbcb24e5&amp;oe=5E982F5D" alt="">
+        </div>
+    </div>
+
+    
+     
+    
+
 </div>
 @endsection
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -81,7 +81,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Community
+                    Laravel
                 </div>
 
                 <div class="links">
