7c28880c4891037c070edc191fb44e58609f6355
Cai heng <ianyaheng822@gmail.com>
2020-03-05 16:56:42 +1100

    amend the user table

---
+++ app/Http/Controllers/PorfilesController.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class PorfilesController extends Controller
+{
+    //
+
+    public function index()
+    {
+        return view('home');
+    }
+}
